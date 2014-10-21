ce4986c874828b044ed982310be3df57c6033882
Shruti Sharma <shrutijalewar@gmail.com>
2014-10-21 10:55:30 -0500

    virus function

--- www/js/virus.js
+++ www/js/virus.js
@@ -1,6 +1,25 @@
 /* exported Virus */
 var Virus = (function(){
   'use strict';
-  Virus.prototype.create = function(){};
-  Virus.prototype.bounce = function(){};
+
+
+  function Virus(x, y){
+    var coordinates = [-3, -2, -1, 0, 1, 2, 3],
+        iX = Math.floor(Math.random * coordinates.length),
+        iY = Math.floor(Math.random * coordinates.length);
+
+    this.width = 20;
+    this.height = 20;
+    this.dX = coordinates[iX];
+    this.dY = coordinates[iY];
+    this.r = this.width/2;
+  }
+  /*Virus.prototype.create = function(){
+
+
+  };
+  Virus.prototype.bounce = function(vX, vY){
+    if(vX )
+
+  };*/
 })();
