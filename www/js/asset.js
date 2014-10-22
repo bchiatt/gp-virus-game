b35bcaef2f5a63402b872d74170ec64fbed678f5
Brian Hiatt <brian.hiatt41@gmail.com>
2014-10-22 13:59:39 -0500

    audio files now check device

--- www/js/asset.js
+++ www/js/asset.js
@@ -11,22 +11,30 @@ var Asset = (function(){
     var asset = {};
 
     asset.fighter = new Image();
-    asset.fighter.src = '/android_asset/www/assets/img/fighter.png';
+    asset.fighter.src = getMediaURL('assets/img/fighter.png');
     asset.weapon = new Image();
-    asset.weapon.src = '/android_asset/www/assets/img/greenLaserRay.png';
+    asset.weapon.src = getMediaURL('assets/img/greenLaserRay.png');
     asset.virus = new Image();
-    asset.virus.src = '/android_asset/www/assets/img/influenza.png';
+    asset.virus.src = getMediaURL('assets/img/influenza.png');
     asset.gameOver = new Media();
-    asset.gameOver.src = '/android_asset/www/assets/audio/dead.wav';
+    asset.gameOver.src = getMediaURL('assets/audio/dead.wav');
     asset.killed = new Media();
-    asset.killed.src = '/android_asset/www/assets/audio/killed.wav';
+    asset.killed.src = getMediaURL('assets/audio/killed.wav');
     asset.shooter = new Media();
-    asset.shooter.src = '/android_asset/www/assets/audio/shooter.wav';
+    asset.shooter.src = getMediaURL('assets/audio/shooter.wav');
     asset.theme = new Media();
-    asset.theme.src = '/android_asset/www/assets/audio/VirusInvaders.mp3';
+    asset.theme.src = getMediaURL('assets/audio/VirusInvaders.mp3');
 
     return asset;
   };
+
+  function getMediaURL(url){
+    if(window.device.platform.toLowerCase() === 'android'){
+      return '/android_asset/www/' + url;
+    }else{
+      return url;
+    }
+  }
 
   return Asset;
 })();
